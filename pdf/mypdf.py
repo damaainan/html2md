2b0621b997af8973428e8d75749eb6313cca7f19
damaainan <damaainan@163.com>
2020-12-14 17:22:14 +0800

    cmd pdf

--- pdf/mypdf.py
+++ pdf/mypdf.py
@@ -6,33 +6,73 @@ import platform
 
 class GenPdf():
     def deal(self, url, title, path):
+        title = title.replace("|", "").replace(' ','')
+        print(title)
         res = requests.get(url)
         # data-src替换为src 有时候返回的正文被隐藏了，将hidden去掉
         html = res.text.replace("data-src", "src").replace('style="visibility: hidden;"',"")
-        html = html.replace("font-size: 16px;font-family: 微软雅黑, sans-serif;letter-spacing: 2px;",'font-size: 20px;font-family: 微软雅黑, sans-serif;letter-spacing: 0px;')
+        # html = html.replace("font-size: 16px;font-family: 微软雅黑, sans-serif;letter-spacing: 2px;",'font-size: 20px;font-family: 微软雅黑, sans-serif;letter-spacing: 0px;')
 
-        soup = BeautifulSoup(html)
+        soup = BeautifulSoup(html, features="lxml")
         # 选择正文（去除javascrapt等）
         html = soup.select('div#img-content')[0]
 
+        # 获取页面样式
+        css = soup.select('head style')
+        # print(type(css))
+        fo=open("./weui.css","r+")
+        weui=fo.read()
+        fo.close()
+
+        font = '''<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>{title}</title>
+</head>
+
+            '''
+
         # 可以修改字体
-        font = '''
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <title>{title}</title>
-        </head>
-        <style type="text/css">
-             @font-face{font-family: "微软雅黑";src:url("‪C:\\Windows\\Fonts\\msyh.ttc")
-        </style>
-         <style type = "text/css">
-            p { font-size:20px;font-family: "微软雅黑","Helvetica Neue", Helvetica, "Hiragino Sans GB", "Microsoft YaHei", Arial, sans-serif, cursive; margin-left: 8px;margin-right: 8px;line-height: 1.2em;}
-            span{font-size: 20px;font-family: "楷体","微软雅黑", sans-serif;letter-spacing: 0px;}
-        </style>
-        <body>
-        '''
-        html = font + str(html) + '</body></html>'
+        # font = '''
+        # <!DOCTYPE html>
+        # <html lang="en">
+        # <head>
+        #     <meta charset="UTF-8">
+        #     <title>{title}</title>
+        # </head>
+        # <style type="text/css">
+        #      @font-face{font-family: "微软雅黑";src:url("‪C:\\Windows\\Fonts\\msyh.ttc")
+        # </style>
+        #  <style type = "text/css">
+        #     p { font-size:20px;font-family: "微软雅黑","Helvetica Neue", Helvetica, "Hiragino Sans GB", "Microsoft YaHei", Arial, sans-serif, cursive; margin-left: 8px;margin-right: 8px;line-height: 1.2em;}
+        #     span{font-size: 20px;font-family: "楷体","微软雅黑", sans-serif;letter-spacing: 0px;}
+        # </style>
+        # <body>
+        # '''
+
+        font=font.format(title=title)+weui
+        for cs in range(len(css)):
+            # print(type(css[cs].get_text()))
+            font = font + css[cs].get_text()
+
+        html = font + "<body>" + str(html) + '</body></html>'
+
+        # 增大较小的字体
+        html=html.replace("font-size: 14px","font-size: 18px").replace("font-size: 12px","font-size: 18px")
+
+
+        rpath = os.path.dirname(os.path.abspath(__file__)) + '/../out/wx/' + path
+        html_path = rpath + "/html/"
+        pdf_path = rpath + "/pdf/"
+        self.mkdir(html_path)
+        self.mkdir(pdf_path)
+        fo=open(html_path +title+'.html',"w+")
+        weui=fo.write(html)
+        fo.close()
+
+
+        # html = font + str(html) + '</body></html>'
 
         # 选项
         options = {
@@ -50,16 +90,21 @@ class GenPdf():
             # 'load-media-error-handling': 'abort',
          }
 
-        # path_wkthmltopdf = r''
-        if platform.system() == "Windows":
-            path_wkthmltopdf = r'C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe'
-        elif platform.system() == "Darwin":
-            path_wkthmltopdf = r''
+        # 由 html 生成pdf
 
-        config = pdfkit.configuration(wkhtmltopdf=path_wkthmltopdf)
-        rpath = os.path.dirname(os.path.abspath(__file__)) + '/../out/wx/' + path
-        self.mkdir(rpath)
-        pdfkit.from_string(str(html), rpath+'/' +title+'.pdf', configuration=config, options=options)
+        os.system('wkhtmltopdf --enable-plugins --enable-forms {} {}'.format(html_path +title+'.html', pdf_path +title+'.pdf'))
+
+        # pdfkit 格式不好
+        # path_wkthmltopdf = r''
+        # if platform.system() == "Windows":
+        #     path_wkthmltopdf = r'C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe'
+        # elif platform.system() == "Darwin":
+        #     path_wkthmltopdf = r''
+
+        # config = pdfkit.configuration(wkhtmltopdf=path_wkthmltopdf)
+        # rpath = os.path.dirname(os.path.abspath(__file__)) + '/../out/wx/' + path
+        # self.mkdir(rpath)
+        # pdfkit.from_string(str(html), rpath+'/' +title+'.pdf', configuration=config, options=options)
 
     def mkdir(self, path):
         # 去除首位空格
@@ -69,6 +114,8 @@ class GenPdf():
         isExists=os.path.exists(path)
         # 判断结果
         if not isExists:
+            # os.makedirs(path+"/html")
+            # os.makedirs(path+"/pdf")
             os.makedirs(path)
             print(path+' 创建成功')
 
