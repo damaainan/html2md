08a64e84461c824137d203524275db1876d9518d
damaainan <damaainan@163.com>
2020-12-28 21:57:59 +0800

    win

---
+++ pdf/getPdfw.py
@@ -0,0 +1,62 @@
+# -*- coding=utf-8 -*-
+
+from store import StoreData
+import time
+import sys
+from mypdf import GenPdf
+
+def getPdf():
+    store = StoreData()
+    # 查数据库
+    toPdfList, columns = store.getListFromParam('state=0')
+
+    # print(toPdfList)
+    # print(columns)
+    # print(type(toPdfList))
+    # 修改状态
+    data = []
+    i=0
+    for val in toPdfList:
+        dic = {}
+        for key,name in enumerate(columns):
+            # print(key)
+            # print(name)
+            # print(val[key])
+            dic[name] = val[key]
+        genpdf(dic)
+        data.append(dic)
+        i+=1
+        if i==100:
+            break
+    # print(data)
+    return
+
+def genpdf(data):
+    store = StoreData()
+    # 传值生成pdf
+    pdf = GenPdf()
+    pdf.dealHtml(data['url'],str(data['turn']) + '-' + data['title'],data['folder'])
+    store.updateUrlState(data['id'])
+    return
+
+if len(sys.argv)>1:
+    print("******")
+    url=sys.argv[1]
+    print(url)
+    folder="面试精选"
+    if len(sys.argv) == 3:
+        folder=sys.argv[2]
+    # 传值生成pdf
+    pdf = GenPdf()
+    title=pdf.dealHtml(url,"",folder)
+    store = StoreData()
+    store.addUrl({'link':url,'folder':folder,'title':title,'msgid':'0','turn':0})
+    store.updateUrlStateByMsg()
+else:
+    getPdf()
+    # print(sys.argv[0])
+
+
+
+
+
--- pdf/mypdf.py
+++ pdf/mypdf.py
@@ -329,7 +329,11 @@ class GenPdf():
         return True
 
     def getHTMLText(self, url):
-        driver = webdriver.PhantomJS(executable_path='/Users/jiachunhui/repo/phantomjs/bin/phantomjs')  # phantomjs的绝对路径
+        if platform.system() == "Windows":
+            path_config = 'F:\\bin\\phantomjs.exe'
+        elif platform.system() == "Darwin":
+            path_config = '/Users/jiachunhui/repo/phantomjs/bin/phantomjs'
+        driver = webdriver.PhantomJS(executable_path=path_config)  # phantomjs的绝对路径
         time.sleep(10)
         driver.get(url)  # 获取网页
         time.sleep(2)
@@ -345,3 +349,136 @@ class GenPdf():
             name=href.split('/')[-2][32:]
             return name+".png"
         return ""
+
+    # 此方法 html 效果更好
+    def dealHtml(self, url, title, path):
+        title = title.replace("|", "").replace(' ','').replace('｜','').replace('?','？').replace('/','-').replace('"','')
+        print(title)
+
+        ## 方法一
+        # res = requests.get(url)
+        # # data-src替换为src 有时候返回的正文被隐藏了，将hidden去掉
+        # html = res.text.replace("data-src", "src").replace('style="visibility: hidden;"',"")
+        # html = html.replace("font-size: 16px;font-family: 微软雅黑, sans-serif;letter-spacing: 2px;",'font-size: 20px;font-family: 微软雅黑, sans-serif;letter-spacing: 0px;')
+
+        ## 方法二
+        htmlstr=self.getHTMLText(url)
+        htmlstr = htmlstr.replace("data-src", "src").replace('style="visibility: hidden;"',"").replace('crossorigin="anonymous"','')
+        htmlstr=re.sub(r'height: [0-9]{1,4}\.{0,1}[0-9]{0,17}px !important;', '', htmlstr)
+        htmlstr=re.sub(r'width: [0-9]{1,4}\.{0,1}[0-9]{0,17}px !important;', '', htmlstr)
+
+        soup = BeautifulSoup(htmlstr, features="lxml")
+        # 选择正文（去除javascrapt等）
+        fhtml = soup.select('div#img-content')[0]
+        if title == "":
+            title = soup.select('#activity-name')[0].get_text()
+            # print(title)
+            title = title.replace("|", "").replace("/", "-").replace(' ','').replace('｜','').replace('?','？').replace("\n",'').replace("\r",'')
+            # print("****")
+            # print(title)
+            # return
+
+        imgs = soup.select('img')
+        # print(imgs)
+
+        imgDict = {}
+        for im in range(len(imgs)):
+            # print(imgs[im])
+            if imgs[im].get('src'):
+                src=imgs[im]['src']
+                # 处理成本地文件名
+                newsrc=self.getLocalImg(src)
+                imgDict[src]=newsrc
+
+        # print(imgDict)
+
+        # 获取页面样式
+        # css = soup.select('head style')
+        # print(len(css))
+        # for cs in range(len(css)):
+        #     print(len(css[cs].get_text()))
+
+
+        css=self.getHtmlByXpath(htmlstr,"//style")
+        # for i in range(len(css)):
+        #     print(len(html.tostring(css[i])))
+
+
+        # return
+        fo=open("./weui.css","r+")
+        weui=fo.read()
+        fo.close()
+
+        font = '''<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>{title}</title>
+'''
+
+        font=font.format(title=title)+weui
+        for cs in range(len(css)):
+            # font = font + "<style>" + css[cs].get_text() + "</style>"
+            font = font + "<style>" + HTMLParser().unescape(html.tostring(css[cs]).decode()) + "</style>"
+            # HTMLParser().unescape(str1.decode())
+
+        fhtml = font + '</head><body>' + str(fhtml) + '</body></html>'
+
+        # 增大较小的字体
+        fhtml=re.sub(r"font-size: 1[0-5]\.{0,1}[0-9]{0,1}[0-9]{0,1}px;",'font-size: 16px;',fhtml)
+
+        rpath = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/out/wx/' + path
+        html_path = rpath + "/html/"
+        pdf_path = rpath + "/pdf/"
+        self.mkdir(html_path)
+        self.mkdir(html_path+"pic/")
+        self.mkdir(pdf_path)
+        fo=open(html_path +title+'.html',"w+",encoding="utf-8")
+        weui=fo.write(fhtml)
+        fo.close()
+
+
+        # html = font + str(html) + '</body></html>'
+
+        # 选项
+        options = {
+            'page-size': 'A4',
+            'margin-top': '0.25in',
+            'margin-right': '0.25in',
+            'margin-bottom': '0.25in',
+            'margin-left': '0.25in',
+            'encoding': "UTF-8",
+            'dpi':1000,
+            # 'custom-header': headers,
+            # 'debug-javascript': [''],
+            'javascript-delay': 10000,
+            # 'no-stop-slow-scripts': "",
+            # 'load-media-error-handling': 'abort',
+         }
+
+        # 由 html 生成pdf
+        # print(html_path +title+'.html')
+        # print(pdf_path+title+'.pdf')
+        ntitle=title.replace('"','\\"')
+
+        hfile=html_path +ntitle+'.html'
+        pfile=pdf_path +ntitle+'.pdf'
+        os.system('wkhtmltopdf --dpi 300 --enable-plugins --enable-forms "{}" "{}"'.format(hfile, pfile))
+        # os.system('weasyprint "{}" "{}"'.format(hfile, pfile))
+
+
+        # TODO 增加功能  把html图片下载到本地并替换 保留 html 获得较好的阅读体验
+        for k in imgDict:
+            if imgDict[k] != "":
+                image=requests.get(k).content
+                with open(html_path+"/pic/"+imgDict[k],"wb") as fp:
+                    fp.write(image)
+
+            fhtml=fhtml.replace(k,"./pic/"+imgDict[k])
+
+        fhtml=fhtml.replace('<body>','<body style="margin:40px;">')
+        fo=open(html_path +title+'.html',"w+",encoding="utf-8")
+        weui=fo.write(fhtml)
+        fo.close()
+
+        return title
