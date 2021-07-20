ac37fbef9fc6b66aaf76b778b006db808bc2281b
damaainan <damaainan@163.com>
2021-07-20 17:19:05 +0800

    bilibili video

--- pdf/to_pdf.py
+++ pdf/to_pdf.py
@@ -5,6 +5,7 @@ from zhihu import zstore
 from wechat import getPdf
 from wechat import store
 from wechat import list as wList
+from bili import list as bList
 from juejin import jgetPdf
 from juejin import jlist
 from juejin import jstore
@@ -35,6 +36,10 @@ def dealAll(**kwargs):
                 getPdf.wxPdf(url=url, folder=folder)
             else:
                 getPdf.wxPdf(url=url)
+                zgetPdf.zhPdf(url=url)
+    elif url.find("bilibili") > -1:
+        bList.deal(url)
+        
     elif url.find("juejin") > -1:
         if url.find("collection") > 1 or url.find('posts') > -1:
             jlist.deal(url)
